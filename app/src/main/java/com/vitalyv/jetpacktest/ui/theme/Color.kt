837aa2ee15382c3081d0c9b69c974dbb1c5e2dba
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-07-23 13:52:01 +0300

    Initial commit

---
+++ app/src/main/java/com/vitalyv/jetpacktest/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.vitalyv.jetpacktest.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF2D9464)
+val PurpleGrey80 = Color(0xFF2EFAD3)
+val Pink80 = Color(0xFF00F089)
+
+val Purple40 = Color(0xFF388E3C)
+val PurpleGrey40 = Color(0xFF75D379)
+val Pink40 = Color(0xFFB0EBC7)
